48bcd1e50561e0513ef983103609ffed20f06fbc
jianrry <git@jianrry.com>
2023-03-29 23:26:55 +0800

    提交信息

--- dst_start.sh
+++ dst_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /steamcmd && ./steamcmd.sh +force_install_dir /dst +login anonymous +app_update 343050 validate +quit
+cd $steamcmd_install_dir && ./steamcmd.sh +force_install_dir $dst_install_dir +login anonymous +app_update 343050 validate +quit
 
-cd /dst/bin64 && \
+cd $dst_install_dir/bin64 && \
 ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Master && \
 ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Caves
