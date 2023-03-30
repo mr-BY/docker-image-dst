73accc4217f727bb251607d2c49bf451b10d0818
jianrry <git@jianrry.com>
2023-03-30 12:14:41 +0800

    提交信息

--- dst_start.sh
+++ dst_start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-cd $dst_install_dir/bin64 && \
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Master && \
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Caves
+# start Don't Starve Together
+dst_start(){
+
+	cd $dst_install_dir/bin64
+	
+	./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Master
+	
+	./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster MyDediServer -shard Caves
+
+}
+
+dst_start
